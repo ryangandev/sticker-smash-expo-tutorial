227d2cdbcf2cf44c74217458bde87aef7a63cce8
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-07-02 11:31:23 -0400

    zoom out sticker size when double tapped on zoomed in

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -18,6 +18,8 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
         onActive: () => {
             if (scaleImage.value !== imageSize * 2) {
                 scaleImage.value = scaleImage.value * 2;
+            } else {
+                scaleImage.value = imageSize
             }
         }
     })
